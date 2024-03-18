7cdd94bfdd0def1c0b5014723fe9475d5f379675
sinavac <93263707+sinavac@users.noreply.github.com>
2024-03-18 12:53:17 -0700

    Update hoki.php

--- hoki.php
+++ hoki.php
@@ -1,1 +1,3 @@
 <a style="display:none"href="https://perpustakaan.umk.ac.id/images/">https://perpustakaan.umk.ac.id/images/</a>
+<a style="display:none"href="https://timnastoto.tesca.id">TIMNASTOTO</a>
+
